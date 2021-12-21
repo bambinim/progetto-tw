8723f75dd61b931b462ceb01a06f23616da49b72
oshodiS <shola.oshodi@studio.unibo.it>
2021-12-21 10:16:13 +0100

    Aggiunte entita

---
+++ database/Entities/Category.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Database\Entities;
+
+use App\Database\Entity;
+
+class Category extends Entity
+{
+
+    private int $id;
+    private string $name;
+    private string $color;
+    private int $imageId;
+   
+
+   /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+  /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+  /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+  /**
+     * @param string $color
+     */
+    public function setColor(string $color): void
+    {
+        $this->color = $color;
+    }
+
+
+    /**
+     * @return int
+     */
+    public function getImageId(): int
+    {
+        return $this->imageId;
+    }
+
+    /**
+     * @param int $imageId
+     */
+    public function setImageId(int $imageId): void
+    {
+        $this->imageId = $imageId;
+    }
+    
+
+
+    
+
+
+    public static function _getColumns(): array
+    {
+        return ['id', 'name', 'color', 'image_id'];
+    }
+
+    public static function _getPrimaryKeyColName(): string
+    {
+        return 'id';
+    }
+
+    public static function _getTableName(): int
+    {
+        return 'categories';
+    }
+}
---
+++ database/Entities/Notification.php
@@ -0,0 +1,99 @@
+
+<?php
+
+namespace App\Database\Entities;
+
+use App\Database\Entity;
+
+class Notification extends Entity
+{
+
+    private int $id;
+    private string $title;
+    private ?string $text = 0;
+    private int $userId;
+
+   
+
+   /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+     /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+  /**
+     * @param string $title
+     */
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+      /**
+     * @param string|null $text
+     */
+    public function setText(?string $text): void
+    {
+        $this->text = $text;
+    }
+
+    /**
+     * @return int
+     */
+    public function getUserId(): int
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @param int $userId
+     */
+    public function setUserId(int $userId): void
+    {
+        $this->userId = $userId;
+    }
+
+    
+
+
+    public static function _getColumns(): array
+    {
+        return ['id','title','text','userId'];
+    }
+
+    public static function _getPrimaryKeyColName(): string
+    {
+        return 'id';
+    }
+
+    public static function _getTableName(): int
+    {
+        return 'notifications';
+    }
+}
