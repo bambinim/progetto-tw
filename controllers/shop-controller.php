47076e20fe8190d6f39ab8d45ee4554110f9eb65
guariglia <daniel.guariglia@studio.unibo.it>
2022-01-23 15:39:15 +0100

    Aggiunta nav-tag per shop, sistemato layout reviews shop e controller

--- controllers/shop-controller.php
+++ controllers/shop-controller.php
@@ -7,37 +7,21 @@ use App\Database\Entities\Shop;
 use App\Database\Entities\User;
 
 if (!empty($router)) {
-    $router->get('/recensioni', function() {
-        if (SecurityManager::isUserLogged()) {
-            header('location: /');
-        } else {
-//momentaneamente disattivato il controllo di login
-            //prendo lo shop dell'utente loggato
-        $shop = Database::getRepository(Shop::class)->findOne(['user_id'=>2/*$_SESSION['uid']*/]);
-        $shop = $shop->toArray();
-        //prendo tutte le recensioni di quello shop
-        $reviews = Database::getRepository(Review::class)->findAll(['shop_id' => $shop['id']]);
-        $res = array();
-        //per ogni recensione Aggiungo il nome di chi l'ha scritta
-        foreach($reviews as $review){
-            $user = Database::getRepository(User::class)->findOne(['id'=>$review->getUserId()]);
-            $user = $user->toArray();
-            print_r($user['first_name']);
-            //$username = $user[fir]." ".$user->getLastName();
-           // array_push($res, $review->toArray(), array('username' => $username));
-        }
-        print_r($res);
-            $template = [
-                'title' => 'Recensioni negozio',
-                'template' => 'shop/review.php',
-                'css' => [''],
-                'reviews' => Database::getRepository(Review::class)->findAll(['shop_id' => $shop['id']])
-            ];
-            require_once(PROJECT_ROOT . '/templates/base.php');
-        }
-    }/*, 'ROLE_USER'*/ );
+    $router->get('/shop/reviews', function () {
+        //get user's shop 
+        $shop =SecurityManager::getUser()->getShop();
+        //get all shop's reviews
+        $reviews = Database::getRepository(Review::class)->findAll(['shop_id' => $shop->getId()]);
+        $template = [
+            'title' => 'Recensioni negozio',
+            'template' => 'shop/review.php',
+            'css' => [''],
+            'reviews' => $reviews
+        ];
+        require_once(PROJECT_ROOT . '/templates/base.php');
+    }, 'ROLE_SELLER');
 
-    $router->get('/shop/products/new', function() {
+    $router->get('/shop/products/new', function () {
         $template = [
             'title' => 'Aggiungi Prodotto',
             'template' => 'shop/product-new.php'
@@ -45,4 +29,3 @@ if (!empty($router)) {
         require_once(PROJECT_ROOT . '/templates/base.php');
     }, 'ROLE_SELLER');
 }
-?>
---
+++ templates/shop/shop-nav-tab.php
@@ -0,0 +1,14 @@
+<ul class="nav nav-tabs d-flex">
+    <li class="nav-item">
+        <a class="nav-link <?php if (basename($_SERVER['REQUEST_URI']) == "") {echo "active";} ?>" aria-current="page" href="#">Prodotti</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link <?php if (basename($_SERVER['REQUEST_URI']) == "") {echo "active";} ?>" href="#">Vendite</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link <?php if (basename($_SERVER['REQUEST_URI']) == "reviews") {echo "active";} ?>" href="#">Recensioni</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link <?php if (basename($_SERVER['REQUEST_URI']) == "") {echo "active";} ?>" href="#">Info</a>
+    </li>
+</ul>
