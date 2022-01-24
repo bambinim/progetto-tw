d866032ae2884a58279abb4bceee8d92614f4b86
Matteo Bambini <matteo.bambini00@gmail.com>
2022-01-24 16:56:10 +0100

    Changes made during the call

--- templates/shop/sales.php
+++ templates/shop/sales.php
@@ -1,39 +1,37 @@
-<?php use App\Database\Entities\Order;?>
-<div class="container-fluid mt-3">
-    <h1 class="mt-3 mb-3"><?php $template['title'] ?></h1>
-    <div class="card">
-        <?php require_once(PROJECT_ROOT . '/templates/shop/shop-nav-tab.php'); ?>
-        <div class="container">
-            <div class="row mt-3">
-                <?php foreach ($template['orders'] as $order) : ?>
+<?php use App\Database\Entities\Order; ?>
+<h1 class="mt-3 mb-3"><?php $template['title'] ?></h1>
+<div class="card">
+    <?php require_once(PROJECT_ROOT . '/templates/shop/shop-nav-tab.php'); ?>
+    <div class="container">
+        <div class="row mt-3">
+            <?php foreach ($template['orders'] as $order) : ?>
 
-                    <div class="col-md-4 mt-3 mb-3">
-                        <div class="row">
+                <div class="col-md-4 mt-3 mb-3">
+                    <div class="row">
 
-                            <div class="col-md-3">
-                                <h3><?php echo $order->getId(); ?></h3>
-                                <p><?php echo Order::_getStatusAsString($order->getStatus());?></p>
-                                <p><?php echo substr($order->getDate(), 0, 10); ?></p>
-                                <p><?php echo $order->getTotal(); ?></p>
-                            </div>
-                            <div class="col">
-                                <a href="#">
-                                    <h3>Prodotti ordinati</h3>
-                                </a>
-                                <?php foreach ($order->getProducts() as $product) : ?>
-                                    <p>- <?php echo $product->getName(); ?></p>
-                                <?php endforeach; ?>
-                            </div>
-                            <div class="col-md-2">
-                                <a href="#">
-                                    <span class="fas fa-chevron-right span-arrow d-none d-lg-inline-block"></span>
-                                </a>
-                            </div>
+                        <div class="col-md-3">
+                            <h3><?php echo $order->getId(); ?></h3>
+                            <p><?php echo Order::_getStatusAsString($order->getStatus()); ?></p>
+                            <p><?php echo substr($order->getDate(), 0, 10); ?></p>
+                            <p><?php echo $order->getTotal(); ?></p>
                         </div>
-                        <hr>
+                        <div class="col">
+                            <a href="#">
+                                <h3>Prodotti ordinati</h3>
+                            </a>
+                            <?php foreach ($order->getProducts() as $product) : ?>
+                                <p>- <?php echo $product->getName(); ?></p>
+                            <?php endforeach; ?>
+                        </div>
+                        <div class="col-md-2">
+                            <a href="#">
+                                <span class="fas fa-chevron-right span-arrow d-none d-lg-inline-block"></span>
+                            </a>
+                        </div>
                     </div>
-                <?php endforeach; ?>
-            </div>
+                    <hr>
+                </div>
+            <?php endforeach; ?>
         </div>
     </div>
 </div>
