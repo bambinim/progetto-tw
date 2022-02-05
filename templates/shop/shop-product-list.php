1285b6e43eac024461d0b608906bcbf9b69752a6
oshodiS <shola.oshodi@studio.unibo.it>
2022-02-05 14:38:03 +0100

    added images product list

--- templates/shop/shop-product-list.php
+++ templates/shop/shop-product-list.php
@@ -4,13 +4,18 @@
    <?php require_once(PROJECT_ROOT . '/templates/shop/shop-nav-tab.php'); ?>
    <div class="container">
       <div class="row mt-3">
-         <?php foreach ($template['products'] as $product) : ?>
-         <div class="col-md-4 md-mb-3">
+         <?php 
+         foreach ($template['products'] as $product) : ?>
+         <div class="col-md-4 md-mb-3 products">
             
                <div class="row prodotto">
+                   <?php $images=$product->getImages();
+                   ?>
                   <div class="col-2">
-                     <img src="\img\purple.jpg">
-                  </div>
+                  <img <?php 
+                  echo("src=/images/get?id=".$images[0]->getID());
+                  ?> alt="...">
+                </div>
                   <div class="col-6 items">
                      <h3><?php echo ($product->getName()); ?></h3>
                    <div class="row">
