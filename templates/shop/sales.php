d7746b6b08e57f1707207c68e0f02726ce87bd6d
guariglia <daniel.guariglia@studio.unibo.it>
2022-02-11 15:53:22 +0100

    Fix Daniel's code
    Sales page:
     -Tolti >
     -Data formattata
     -Prezzo formattato
    
    User/orders page:
     -Card allargate
     -informazioni in grassetto riviste
     -Prezzi e date formattati
    
    Reviews page:
     -Rimosso container esterno

--- templates/shop/sales.php
+++ templates/shop/sales.php
@@ -6,27 +6,22 @@
         <div class="row mt-3">
             <?php foreach ($template['orders'] as $order) : ?>
 
-                <div class="col-md-4 mt-3 mb-3">
+                <div class="col-lg-4 mt-3 mb-3">
                     <div class="row">
 
-                        <div class="col-md-3">
-                            <h3><?php echo $order->getId(); ?></h3>
-                            <p class=""><?php echo $order->getStatusAsString(); ?></p>
-                            <p><?php echo substr($order->getDate(), 0, 10); ?></p>
-                            <p><?php echo $order->getTotal(); ?></p>
+                        <div class="col-lg-3">
+                            <p class="fs-3"><?php echo $order->getId(); ?></p>
+                            <p class="text-nowrap "><?php echo $order->getStatusAsString(); ?></p>
+                            <p class=""><?php echo date_format(new DateTime($order->getDate()), 'd/m/Y'); ?></p>
+                            <p><?php echo number_format($order->getTotal(), 2); ?></p>
                         </div>
                         <div class="col">
                             <a href="#">
-                                <h3>Prodotti ordinati</h3>
+                                <p class="fs-2 text-nowrap">Prodotti ordinati</p>
                             </a>
                             <?php foreach ($order->getProducts() as $product) : ?>
                                 <p>- <?php echo $product->getName(); ?></p>
                             <?php endforeach; ?>
-                        </div>
-                        <div class="col-md-2">
-                            <a href="#">
-                                <span class="fas fa-chevron-right span-arrow d-none d-lg-inline-block"></span>
-                            </a>
                         </div>
                     </div>
                     <hr>
