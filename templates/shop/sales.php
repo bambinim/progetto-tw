8c4a2ceb5be5b160d647dbb696f5c413e0f3b2c4
guariglia <daniel.guariglia@studio.unibo.it>
2022-02-12 10:23:10 +0100

    fix

--- templates/shop/sales.php
+++ templates/shop/sales.php
@@ -3,14 +3,14 @@
     <?php require_once(PROJECT_ROOT . '/templates/shop/shop-nav-tab.php'); ?>
     <div class="container">
         <div class="row mt-3">
+        <h1 class="mt-3 mb-5 ms-3"><?php echo $template['title']; ?></h1>
             <?php foreach ($template['orders'] as $order) : ?>
-
                 <div class="col-lg-4 mt-3 mb-3">
                     <div class="row">
                         <div class="col-lg-3">
-                            <p class="fs-3"><?php echo $order->getId(); ?></p>
-                            <p class="text-nowrap "><?php echo $order->getStatusAsString(); ?></p>
-                            <p class=""><?php echo date_format(new DateTime($order->getDate()), 'd/m/Y'); ?></p>
+                            <p class="fs-3">ID: <?php echo $order->getId(); ?></p>
+                            <p class="text-nowrap"><?php echo $order->getStatusAsString(); ?></p>
+                            <p class="text-nowrap"><?php echo date_format(new DateTime($order->getDate()), 'd/m/Y'); ?></p>
                             <p><?php echo number_format($order->getTotal(), 2); ?> €</p>
                         </div>
                         <div class="col">
@@ -18,7 +18,7 @@
                                 <p class="fs-2 text-nowrap">Prodotti ordinati</p>
                             </a>
                             <?php foreach ($order->getProducts() as $product) : ?>
-                                <p>- <?php echo $product->getName(); ?></p>
+                                <a href="/products/view?id=<?php echo $product->getId();?>" class="text-decoration-none">- <?php echo $product->getName(); ?></a>
                             <?php endforeach; ?>
                         </div>
                     </div>
