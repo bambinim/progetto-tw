cf52a8e6bd538004ba503d4df1d2141a845aea34
Matteo Bambini <matteo.bambini00@gmail.com>
2022-02-12 11:57:32 +0100

    Fixed search and orders list pages

--- templates/home/search.php
+++ templates/home/search.php
@@ -1,7 +1,12 @@
 <div class="row mx-3">
-    <?php foreach ($products = $template['products'] as $prod): ?>
-    <div class="col-lg-2 col-12 mt-3">
-        <?= $prod->renderCard(); ?>
-    </div>
-    <?php endforeach; ?>
+    <?php $products = $template['products']; ?>
+    <?php if (count($products) > 0): ?>
+        <?php foreach ($products as $prod): ?>
+        <div class="col-lg-2 col-12 mt-3">
+            <?= $prod->renderCard(); ?>
+        </div>
+        <?php endforeach; ?>
+    <?php else: ?>
+        <h1>Non è stato trovato nessun prodotto</h1>
+    <?php endif; ?>
 </div>
--- templates/user/orders.php
+++ templates/user/orders.php
@@ -1,40 +1,32 @@
 <h1 class="text-center mt-3 mb-4">I miei ordini</h1>
 <div class="row">
-    <?php
-
-    use App\Database\Entities\Order;
-
-    foreach ($template['orders'] as $order) : ?>
-        <div class="col-lg-4 d-flex justify-content-center">
-            <div class="card ps-4 pe-4 mb-2 mx-2">
-                <p class="status-<?php echo $order->getStatus(); ?> fw-bold mt-2"><?php echo $order->getStatusAsString(); ?></p>
+    <?php foreach ($template['orders'] as $order) : ?>
+        <div class="col-12 col-lg-4">
+            <div class="card ps-4 pe-4 m-2">
+                <span class="status-<?php echo $order->getStatus(); ?> fw-bold mt-2"><?php echo $order->getStatusAsString(); ?></span>
                 <hr>
-                <div class="d-flex">
-                    <p class="fw-bold">Numero ordine: &nbsp;</p>
-                    <p> <?php echo $order->getId(); ?></p>
+                <div>
+                    <span class="fw-bold">Numero ordine: &nbsp;</span>
+                    <span> <?php echo $order->getId(); ?></span>
                 </div>
-                <div class="d-flex">
-                    <p class="fw-bold">Data ordine: &nbsp;</p>
-                    <p><?php echo date_format(new DateTime($order->getDate()), 'd/m/Y'); ?></p>
+                <div>
+                    <span class="fw-bold">Data ordine: &nbsp;</span>
+                    <span><?php echo date_format(new DateTime($order->getDate()), 'd/m/Y'); ?></span>
                 </div>
-                <div class="d-flex">
-                    <p class="fw-bold">Totale: &nbsp;</p>
-                    <p><?php echo number_format($order->getTotal(), 2); ?>€</p>
+                <div>
+                    <span class="fw-bold">Totale: &nbsp;</span>
+                    <span><?php echo number_format($order->getTotal(), 2); ?>&euro;</span>
                 </div>
                 <hr>
-                <div class="row">
+                <div class="d-flex flex-row">
                     <?php foreach ($order->getProducts() as $product) : ?>
-                        <div class="col-3">
-                            <?php $imgs = $product->getImages(); ?>
-                            <a href="/products/view?id=<?php echo $product->getId();?>">
-                                <img src=<?php echo "/images/get?id={$imgs[0]->getId()}"; ?> alt="immagine di un prodotto ordinato" class="w-100 mt-2">
-                            </a>
-                        </div>
+                        <a target="_blank" class="product-image" href="/products/view?id=<?php echo $product->getId();?>">
+                            <img src="/images/get?id=<?= $product->getImages()[0]->getId(); ?>" alt="immagine di un prodotto ordinato">
+                        </a>
                     <?php endforeach; ?>
                 </div>
                 <hr>
                 <a href="/user/orders/view?id=<?php echo $order->getId(); ?>" class="btn btn-primary btn-sm w-50 mb-4">Dettagli ordine</a>
-
             </div>
         </div>
     <?php endforeach; ?>
